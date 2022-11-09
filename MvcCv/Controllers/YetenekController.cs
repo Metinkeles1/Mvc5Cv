fa7a8d40a23640363e0448bd01172d05e37e9e80
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-11-10 00:02:55 +0300

    SkillController

--- MvcCv/Controllers/YetenekController.cs
+++ MvcCv/Controllers/YetenekController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 
 namespace MvcCv.Controllers
 {
     public class YetenekController : Controller
     {
-        // GET: Yetenek
+        // GET: Yetenek        
+        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim>();
         public ActionResult Index()
         {
-            return View();
+            var yetenekler = repo.List();
+            return View(yetenekler);
         }
     }
 }
