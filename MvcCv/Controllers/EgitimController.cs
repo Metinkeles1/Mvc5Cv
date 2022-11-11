7c87bd36154919ec878feef73fa21b5466cf5b7e
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-11-11 09:29:34 +0300

    EgitimDeleteUpdate

--- MvcCv/Controllers/EgitimController.cs
+++ MvcCv/Controllers/EgitimController.cs
@@ -32,5 +32,34 @@ namespace MvcCv.Controllers
             repo.TAdd(p);
             return RedirectToAction("Index");
         }
+
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimDuzenle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimDuzenle(TblEgitimlerim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimDuzenle");
+            }
+            var e = repo.Find(x => x.ID == t.ID);
+            e.Baslik = t.Baslik;
+            e.AltBaslik = t.AltBaslik;
+            e.AltBaslik2 = t.AltBaslik2;
+            e.GNO = t.GNO;
+            e.Tarih = t.Tarih;
+            repo.TUpdate(e);
+            return RedirectToAction("Index");
+        }
     }
 }
