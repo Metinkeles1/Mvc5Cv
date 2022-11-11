7b689a25516a97da84fdc61f0031eb7016bfc306
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-11-11 11:20:41 +0300

    SertifikaList

--- MvcCv/Controllers/SertifikaController.cs
+++ MvcCv/Controllers/SertifikaController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 
 namespace MvcCv.Controllers
 {
     public class SertifikaController : Controller
     {
         // GET: Sertifika
+        GenericRepository<TblSertifikalarim> repo = new GenericRepository<TblSertifikalarim>();
         public ActionResult Index()
         {
-            return View();
+            var sertifika = repo.List();
+            return View(sertifika);
         }
     }
 }
