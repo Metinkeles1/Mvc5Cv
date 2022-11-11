0696eab774aa462063365b6b8db352d78f069e66
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-11-11 08:59:26 +0300

    MaxLengthValidationController

--- MvcCv/Controllers/EgitimController.cs
+++ MvcCv/Controllers/EgitimController.cs
@@ -25,6 +25,10 @@ namespace MvcCv.Controllers
         [HttpPost]
         public ActionResult EgitimEkle(TblEgitimlerim p)
         {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
             repo.TAdd(p);
             return RedirectToAction("Index");
         }
