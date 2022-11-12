069627a7459c3a1c1114ce7763599a541931861b
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-11-12 11:14:56 +0300

    HobiEdit

--- MvcCv/Controllers/HobiController.cs
+++ MvcCv/Controllers/HobiController.cs
@@ -12,10 +12,20 @@ namespace MvcCv.Controllers
     {
         // GET: Hobi
         GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+        [HttpGet]
         public ActionResult Index()
         {
             var hobiler = repo.List();
             return View(hobiler );
+        }
+        [HttpPost]
+        public ActionResult Index(TblHobilerim t)
+        {
+            var hobi = repo.Find(x => x.ID == 1);
+            hobi.Aciklama1 = t.Aciklama1;
+            hobi.Aciklama2 = t.Aciklama2;
+            repo.TUpdate(hobi);
+            return RedirectToAction("Index");
         }
     }
 }
